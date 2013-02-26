13d7ed178e9b0841ee1293ad2ff1d1bbc7a7624c
CosWind <xy2491259@gmail.com>
2013-02-26 20:48:32 +0800

    Init the repo.

---
+++ jsonp.js
@@ -0,0 +1,43 @@
+/**
+ * @author Yi xia
+ *
+**/
+;(function(undefined) {
+    function getScript(url, callback) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        
+        script.onload = script.onreadystatechange = function() {
+            if (!this.readyState ||
+                this.readyState === "loaded" || 
+                this.readyState === "complete") {
+                this.onload = this.onreadystatechange = null;
+                document.getElementsByTagName('head')[0].removeChild(this);
+                callback();
+            }
+        };
+        
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+    window.jsonp = function (option, callbackName) {
+        if(!option.url || !callbackName) {
+            return;
+        }
+        var data = option.data || {};
+        
+        data[callbackName] = 'XD' + +new Date();
+        window[data[callbackName]] = function(json) {
+            option.callback(json);
+        };
+        var url = option.url + '?' + param(data);
+        
+        getScript(url, function() {
+             window[data[callbackName]] = undefined;
+             try {
+                 delete window[data[callbackName]];
+             }
+             catch(e) {}
+        });
+    }
+})();
